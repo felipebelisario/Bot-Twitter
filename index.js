1c064e6735b9a55aee00b58f42b20c7bb15cb5c8
Felipe <felipebelisario@ufu.br>
2020-05-04 21:26:12 -0300

    aaaa

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const Twit = require('twit');
+var Twit = require('twit');
 
 require('dotenv').config();
 
+/* Instancie o bot com as chaves no arquivo .env */
 const Bot = new Twit({
     consumer_key: process.env.CONSUMER_KEY,
     consumer_secret: process.env.CONSUMER_SECRET,
@@ -10,102 +11,62 @@ const Bot = new Twit({
     timeout_ms: 60 * 1000,
 });
 
-// start stream and track tweets
-const stream = Bot.stream('statuses/filter', { track: ['@bot_passeiamao'] });
-var contador = 0
+console.log('Este bot está rodando...');
 
-console.log("Bot rodando...")
+var firstId
 
-// use this to log errors from requests
-function responseCallback(err, data, response) {
-    if (err !== undefined)
-        console.log(err);
-    else
-        console.log("Mais uma passada, somando " + contador);
+/* BotInit() : Para iniciar o bot */
+function BotInit() {
+    var query = {
+        q: "from:@labezin",
+        result_type: "recent"
+    }
+
+    Bot.get('search/tweets', query, BotGotLatestTweet);
+
+    function BotGotLatestTweet(error, data, response) {
+        if (error) {
+            console.log('Bot não pôde achar o último tweet: ' + error);
+        }
+        else {
+            var id = data.statuses[0].id_str
+
+            if (firstId === undefined) {
+                firstId = id
+            }
+            else {
+                if (data.statuses[0].id_str !== firstId &&
+                    data.statuses[0].in_reply_to_status_id === null &&
+                    data.statuses[0].in_reply_to_status_id_str === null &&
+                    data.statuses[0].in_reply_to_user_id === null &&
+                    data.statuses[0].in_reply_to_user_id_str === null &&
+                    data.statuses[0].in_reply_to_screen_name === null &&
+                    !data.statuses[0].retweeted_status) {
+
+                    Bot.post('statuses/update', {
+                        status: 'fodase parcero',
+                        in_reply_to_status_id: id,
+                        auto_populate_reply_metadata: true
+                    }, BotCommented);
+
+                    firstId = id
+                }
+            }
+
+            function BotCommented(error, response) {
+                if (error) {
+                    console.log('Bot não pode comentar: ' + error);
+                }
+                else {
+                    console.log('Bot comentou: ' + id);
+                }
+            }
+        }
+    }
 }
 
-// event handler
-stream.on('tweet', tweet => {
-    contador = contador + 1
-    Bot.post('statuses/update', {
-        status: 'Ihaaa você acaba de passar a mão na bunda do LAB!\n\nAgora ela recebeu passadas de mão ' + contador + ' vez(es)',
-        in_reply_to_status_id: tweet.id_str,
-        auto_populate_reply_metadata: true
-    }, responseCallback);
-});
+/* Configure um intervalo de 5 segundos (em microsegundos) */
+setInterval(BotInit, 5 * 1000);
 
-
-/* Bot fodase parcero */
-
-// var Twit = require('twit');
-
-// require('dotenv').config();
-
-// /* Instancie o bot com as chaves no arquivo .env */
-// const Bot = new Twit({
-//     consumer_key: process.env.CONSUMER_KEY,
-//     consumer_secret: process.env.CONSUMER_SECRET,
-//     access_token: process.env.ACCESS_TOKEN,
-//     access_token_secret: process.env.ACCESS_TOKEN_SECRET,
-//     timeout_ms: 60 * 1000,
-// });
-
-// console.log('Este bot está rodando...');
-
-// var firstId
-
-// /* BotInit() : Para iniciar o bot */
-// function BotInit() {
-//     var query = {
-//         q: "from:@labezin",
-//         result_type: "recent"
-//     }
-
-//     Bot.get('search/tweets', query, BotGotLatestTweet);
-
-//     function BotGotLatestTweet(error, data, response) {
-//         if (error) {
-//             console.log('Bot não pôde achar o último tweet: ' + error);
-//         }
-//         else {
-//             var id = data.statuses[0].id_str
-
-//             if (firstId === undefined) {
-//                 firstId = id
-//             }
-//             else {
-//                 if (data.statuses[0].id_str !== firstId &&
-//                     data.statuses[0].in_reply_to_status_id === null &&
-//                     data.statuses[0].in_reply_to_status_id_str === null &&
-//                     data.statuses[0].in_reply_to_user_id === null &&
-//                     data.statuses[0].in_reply_to_user_id_str === null &&
-//                     data.statuses[0].in_reply_to_screen_name === null &&
-//                     !data.statuses[0].retweeted_status) {
-
-//                     Bot.post('statuses/update', {
-//                         status: 'fodase parcero',
-//                         in_reply_to_status_id: id,
-//                         auto_populate_reply_metadata: true
-//                     }, BotCommented);
-
-//                     firstId = id
-//                 }
-//             }
-
-//             function BotCommented(error, response) {
-//                 if (error) {
-//                     console.log('Bot não pode comentar: ' + error);
-//                 }
-//                 else {
-//                     console.log('Bot comentou: ' + id);
-//                 }
-//             }
-//         }
-//     }
-// }
-
-// /* Configure um intervalo de 5 segundos (em microsegundos) */
-// setInterval(BotInit, 5 * 1000);
-
-// /* Inicialize o bot Bot */
-// BotInit();
+/* Inicialize o bot Bot */
+BotInit();
