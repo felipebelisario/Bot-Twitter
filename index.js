62a3f9c31c6f62e165b01d21dc05244dd1ebf4b4
Felipe <felipebelisario@ufu.br>
2020-05-04 22:21:39 -0300

    aaaa

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ stream.on('tweet', tweet => {
     contador = contador + 1
     if (tweet.text.indexOf("@bot_passeiamao") > -1) {
         Bot.post('statuses/update', {
-            status: 'Ihaaa você acaba de passar a mão na bunda do LAB!\n\nAgora ela recebeu passadas de mão ' + contador + ' vez(es)',
+            status: 'Ihaaa você acaba de passar a mão na bunda do LAB! \n\nAgora ela recebeu passadas de mão ' + contador + ' vez(es)',
             in_reply_to_status_id: tweet.id_str,
             auto_populate_reply_metadata: true
         }, responseCallback);
