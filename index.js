80141ab68a434f875f2914058250691af9ff0ab3
Felipe <felipe.belisario@sankhyacode.com>
2020-05-12 22:05:31 -0300

    Bot correios

--- index.js
+++ index.js
@@ -24,50 +24,57 @@ function responseCallback(err, data, response) {
         console.log("Rastreado!");
 }
 
-// event handler
-stream.on('tweet', tweet => {
-    if (!tweet.display_text_range) {
-        Bot.post('statuses/update', {
-            status: 'Não encontrei nenhum número de rastreamento no seu tweet!',
-            in_reply_to_status_id: tweet.id_str,
-            auto_populate_reply_metadata: true
-        }, responseCallback);
-    }
-    else {
-        if ((tweet.text.slice(tweet.display_text_range[0], tweet.display_text_range[1])).indexOf("@bot_rastreioc") > -1) {
-            objectsList = (tweet.text.slice(tweet.display_text_range[0], tweet.display_text_range[1])).split(" ")
+function rastreiaCodigos(objectsList) {
+    TrackingCorreios.track(objectsList)
+        .then((res) => {
+            if(res.length > 0){
+                res.forEach(element => {
+                    text = "-> Nome: " + element.nome + "\n-> Número: " + element.numero +
+                        "\n\n---Status---\n" + element.evento[0].descricao + "Cidade: " + element.evento[0].cidade 
+                        + "\nData: " + element.evento[0].data + "\nHora: " + element.evento[0].hora
 
-            objectsList.splice(objectsList.indexOf("@bot_rastreioc"), 1)
-
-            objectsList.forEach(element => {
-                element.replace(",", "")
-            })
-
-            TrackingCorreios.track(objectsList)
-                .then((res) => {
-                    var text
-
-                    res.forEach(element => {
-                        text = "-> Nome: " + res.nome + "\n-> Número: " + res.numero +
-                            "\n\n---Status---\n" + res.evento[0].descricao + "Cidade: " + res.evento[0].cidade 
-                            + "\nData: " + res.evento[0].data + "\nHora: " + res.evento[0].hora
-
-                        Bot.post('statuses/update', {
-                            status: text,
-                            in_reply_to_status_id: tweet.id_str,
-                            auto_populate_reply_metadata: true
-                        }, responseCallback);
-                    })
-
-
-                })
-                .catch(() => {
                     Bot.post('statuses/update', {
-                        status: "Tivemos um problema com o(s) número(s) de rastreio! Verfique se está(ão) correto(s) e se é(são) rastreável(is) pelos correios",
+                        status: text,
                         in_reply_to_status_id: tweet.id_str,
                         auto_populate_reply_metadata: true
                     }, responseCallback);
                 })
+            }
+            else {
+                Bot.post('statuses/update', {
+                    status: "Não encontrei nenhum número de rastreio no seu tweet!",
+                    in_reply_to_status_id: tweet.id_str,
+                    auto_populate_reply_metadata: true
+                }, responseCallback);
+            }
+
+
+        })
+        .catch(() => {
+            Bot.post('statuses/update', {
+                status: "Tivemos um problema com o(s) número(s) de rastreio! Verfique se está(ão) correto(s) e se é(são) rastreável(is) pelos correios",
+                in_reply_to_status_id: tweet.id_str,
+                auto_populate_reply_metadata: true
+            }, responseCallback);
+        })
+}
+
+// event handler
+stream.on('tweet', tweet => {
+    if (!tweet.display_text_range) {
+        objectsList = (tweet.text).split(" ")
+    } else {
+        if ((tweet.text.slice(tweet.display_text_range[0], tweet.display_text_range[1])).indexOf("@bot_rastreioc") > -1) {
+            objectsList = (tweet.text.slice(tweet.display_text_range[0], tweet.display_text_range[1])).split(" ")
         }
     }
+
+    objectsList.splice(objectsList.indexOf("@bot_rastreioc"), 1)
+
+    objectsList.forEach(element => {
+        element.replace(",", "")
+    })
+
+    rastreiaCodigos(objectsList)
+    
 });
