4dd4c74a6b9b1cdb74344cf38f6482837ea18180
Felipe <felipe.belisario@sankhyacode.com>
2020-05-12 21:46:59 -0300

    Bot correios

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Twit = require('twit');
+const TrackingCorreios = require('tracking-correios')
 
 require('dotenv').config();
 
@@ -11,113 +12,62 @@ const Bot = new Twit({
 });
 
 // start stream and track tweets
-const stream = Bot.stream('statuses/filter', { track: '@bot_passeiamao' });
-var contador = 0
+const stream = Bot.stream('statuses/filter', { track: '@bot_rastreioc' });
 
-console.log("Bot rodando... contador = " + contador)
+console.log("Bot rodando...")
 
 // use this to log errors from requests
 function responseCallback(err, data, response) {
     if (err !== undefined)
         console.log(err);
     else
-        console.log("Mais uma passada, somando " + contador);
+        console.log("Rastreado!");
 }
 
 // event handler
 stream.on('tweet', tweet => {
     if (!tweet.display_text_range) {
-        contador = contador + 1
         Bot.post('statuses/update', {
-            status: 'Ihaaa você acaba de passar a mão na bunda do LAB! \n\nAgora ela recebeu passadas de mão ' + contador + ' vez(es)',
+            status: 'Não encontrei nenhum número de rastreamento no seu tweet!',
             in_reply_to_status_id: tweet.id_str,
             auto_populate_reply_metadata: true
         }, responseCallback);
     }
     else {
-        if ((tweet.text.slice(tweet.display_text_range[0], tweet.display_text_range[1])).indexOf("@bot_passeiamao") > -1) {
-            contador = contador + 1
-            Bot.post('statuses/update', {
-                status: 'Ihaaa você acaba de passar a mão na bunda do LAB! \n\nAgora ela recebeu passadas de mão ' + contador + ' vez(es)',
-                in_reply_to_status_id: tweet.id_str,
-                auto_populate_reply_metadata: true
-            }, responseCallback);
+        if ((tweet.text.slice(tweet.display_text_range[0], tweet.display_text_range[1])).indexOf("@bot_rastreioc") > -1) {
+            objectsList = (tweet.text.slice(tweet.display_text_range[0], tweet.display_text_range[1])).split(" ")
+
+            objectsList.splice(objectsList.indexOf("@bot_rastreioc"), 1)
+
+            objectsList.forEach(element => {
+                element.replace(",", "")
+            })
+
+            TrackingCorreios.track(objectsList)
+                .then((res) => {
+                    var text
+
+                    res.forEach(element => {
+                        text = "-> Nome: " + res.nome + "\n-> Número: " + res.numero +
+                            "\n\n---Status---\n" + res.evento[0].descricao + "Cidade: " + res.evento[0].cidade 
+                            + "\nData: " + res.evento[0].data + "\nHora: " + res.evento[0].hora
+
+                        Bot.post('statuses/update', {
+                            status: text,
+                            in_reply_to_status_id: tweet.id_str,
+                            auto_populate_reply_metadata: true
+                        }, responseCallback);
+                    })
+
+
+                })
+                .catch(() => {
+                    Bot.post('statuses/update', {
+                        status: "Tivemos um problema com o(s) número(s) de rastreio! Verfique se está(ão) correto(s) e se é(são) rastreável(is) pelos correios",
+                        in_reply_to_status_id: tweet.id_str,
+                        auto_populate_reply_metadata: true
+                    }, responseCallback);
+                })
         }
     }
 });
-
-
-/* Bot fodase parcero */
-
-// var Twit = require('twit');
-
-// require('dotenv').config();
-
-// /* Instancie o bot com as chaves no arquivo .env */
-// const Bot = new Twit({
-//     consumer_key: process.env.CONSUMER_KEY,
-//     consumer_secret: process.env.CONSUMER_SECRET,
-//     access_token: process.env.ACCESS_TOKEN,
-//     access_token_secret: process.env.ACCESS_TOKEN_SECRET,
-//     timeout_ms: 60 * 1000,
-// });
-
-// console.log('Este bot está rodando...');
-
-// var firstId
-
-// /* BotInit() : Para iniciar o bot */
-// function BotInit() {
-//     var query = {
-//         q: "from:@labezin",
-//         result_type: "recent"
-//     }
-
-//     Bot.get('search/tweets', query, BotGotLatestTweet);
-
-//     function BotGotLatestTweet(error, data, response) {
-//         if (error) {
-//             console.log('Bot não pôde achar o último tweet: ' + error);
-//         }
-//         else {
-//             var id = data.statuses[0].id_str
-
-//             if (firstId === undefined) {
-//                 firstId = id
-//             }
-//             else {
-//                 if (data.statuses[0].id_str !== firstId &&
-//                     data.statuses[0].in_reply_to_status_id === null &&
-//                     data.statuses[0].in_reply_to_status_id_str === null &&
-//                     data.statuses[0].in_reply_to_user_id === null &&
-//                     data.statuses[0].in_reply_to_user_id_str === null &&
-//                     data.statuses[0].in_reply_to_screen_name === null &&
-//                     !data.statuses[0].retweeted_status) {
-
-//                     Bot.post('statuses/update', {
-//                         status: 'fodase parcero',
-//                         in_reply_to_status_id: id,
-//                         auto_populate_reply_metadata: true
-//                     }, BotCommented);
-
-//                     firstId = id
-//                 }
-//             }
-
-//             function BotCommented(error, response) {
-//                 if (error) {
-//                     console.log('Bot não pode comentar: ' + error);
-//                 }
-//                 else {
-//                     console.log('Bot comentou: ' + id);
-//                 }
-//             }
-//         }
-//     }
-// }
-
-// /* Configure um intervalo de 5 segundos (em microsegundos) */
-// setInterval(BotInit, 5 * 1000);
-
-// /* Inicialize o bot Bot */
-// BotInit();
